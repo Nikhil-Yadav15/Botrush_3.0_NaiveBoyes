d02a903f8018a90b953c4cc8914fe1249aff8ace
Tirth Patel <2024tirthpatel@gmail.com>
2025-04-18 22:40:21 +0530

    Update Navbar.js

--- NextApp/app/components/Navbar.js
+++ NextApp/app/components/Navbar.js
@@ -4,12 +4,16 @@ import Link from "next/link";
 import "@/app/globals.css";
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-md">
-        <div className="container mx-auto">
-            <Link href="/" className="flex items-center p-2">
-              <span className="text-2xl font-bold text-gray-800">hacktivate</span>
-            </Link>
-        </div>
+    <nav className="shadow-sm py-3 relative isolate">
+      <div
+        className="absolute inset-0 -z-10 opacity-[1] bg-gradient-to-br from-black/20 to-purple-600/10"
+      />
+      <div className="container mx-auto w-full flex items-center justify-center">
+        <img src="./icon.svg" className="nabarLogo"/>
+        <Link href="/" className="flex items-center p-2">
+        <span style={{ fontFamily: 'var(--font-chakra)', fontSize: '2rem' }} className="w-full text-2xl text-center text font-bold text-white text-shadow-blue-600 ">Naive Boyes</span>
+        </Link>
+      </div>
     </nav>
   );
 };
